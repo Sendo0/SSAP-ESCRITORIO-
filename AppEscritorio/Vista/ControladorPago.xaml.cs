7678abe47062f630bdd2fb273c1fe3e5ee86065a
unknown <vicente.daniel.puebla@gmail.com>
2022-10-13 23:19:42 -0300

    Control de pagos añadido

--- AppEscritorio/Vista/ControladorPago.xaml.cs
+++ AppEscritorio/Vista/ControladorPago.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Vista
 {
     /// <summary>
     /// Lógica de interacción para ControladorPago.xaml
     /// </summary>
-    public partial class ControladorPago : Window
+    public partial class ControladorPago : MetroWindow
     {
         public ControladorPago()
         {
